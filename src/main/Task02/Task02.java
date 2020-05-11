3e8f288db0ef9412a2b57cfcd8a39ac9833dd797
Savelev Alexander <rnd.askella@gmail.com>
2020-05-11 19:11:48 +0300

    Correction task after remarks from E.S.

--- src/main/Task02/Task02.java
+++ src/main/Task02/Task02.java
@@ -5,26 +5,32 @@ import java.util.Collections;
 import java.util.List;
 
 public class Task02 {
-    static void returnIndexMethod(Integer[] array, int NumberSum) {
-        int IndexNumbers = 0;
+    static Integer[] returnIndexMethod(Integer[] array, int NumberSum) {
+        Integer[] containsIndex = new Integer[2];
 
         List<Integer> list = new ArrayList<>();
         Collections.addAll(list, array);
 
         for (int i = 0; i < list.size(); i++) {
-            int Numbers;
-            Numbers = NumberSum - list.get(i);
-            if (list.contains(Numbers)) {
-                IndexNumbers = list.indexOf(list.get(i));
-                System.out.print(IndexNumbers + " ");
+            int numberTwo;
+            int numberOne = list.get(i);
+            numberTwo = NumberSum - numberOne;
+            if (list.contains(numberTwo)) {
+                containsIndex[0] = list.indexOf(numberOne);
+                containsIndex[1] = list.indexOf(numberTwo);
             }
         }
+        return containsIndex;
     }
 
 
     public static void main(String[] args) {
         Integer[] array = {3, 8, 15, 17};
-        int number = 20;
+        int number = 23;
         returnIndexMethod(array, number);
+        for (Integer i :
+        returnIndexMethod(array, number)) {
+            System.out.print(i + " ");
+        }
     }
 }
